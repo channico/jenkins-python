6ee90b230c9a3a0ce8810fd55643806e96f3d942
Nicodemus Chan <nicodemus.chan@aquariux.com>
2024-06-19 15:34:51 +0800

    Initial commit with source code and tests

---
+++ src/my_module/my_class.py
@@ -0,0 +1,11 @@
+# src/my_module/my_class.py
+
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"
+
+    def add(self, a, b):
+        return a + b
---
+++ tests/test_my_class.py
@@ -0,0 +1,21 @@
+# tests/test_my_class.py
+
+import unittest
+from src.my_module.my_class import MyClass
+
+
+class TestMyClass(unittest.TestCase):
+    def setUp(self):
+        self.my_class = MyClass("World")
+
+    def test_greet(self):
+        self.assertEqual(self.my_class.greet(), "Hello, World!")
+
+    def test_add(self):
+        self.assertEqual(self.my_class.add(1, 2), 3)
+        self.assertEqual(self.my_class.add(-1, 1), 0)
+        self.assertEqual(self.my_class.add(-1, -1), -2)
+
+
+if __name__ == '__main__':
+    unittest.main()
