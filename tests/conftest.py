bd0be27ea2e541cbcc479f797843b95007b0e5ac
Nicodemus Chan <nicodemus@phonester.com>
2024-07-03 23:33:51 +0800

    Refactor test_google.py to use conftest.py and allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,36 @@
 import sys
 import os
 
+import allure
+import pytest
+from selenium import webdriver
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+
+@pytest.fixture
+def driver():
+    # Initialize the WebDriver
+    options = webdriver.ChromeOptions()
+    # options.add_argument("headless")  # Headless mode - browser with UI (faster)
+    # options.add_experimental_option("detach", True)  # Leave browser open
+
+    # Connect to Selenium Hub
+    # Instead of localhost, change it to the name of the server running Selenium hub
+    if 'SELENIUM_URL' in os.environ:
+        hub_url = os.environ['SELENIUM_URL']
+        print(f"Hub URL: {hub_url}")
+        driver = webdriver.Remote(command_executor=hub_url, options=options)
+    else:
+        driver = webdriver.Chrome(options=options)
+
+    yield driver
+    # Quit the WebDriver
+    driver.quit()
+
+
+def pytest_exception_interact(node, call, report):
+    if report.failed:
+        driver = node.funcargs['driver']
+        screenshot = driver.get_screenshot_as_png()
+        allure.attach(screenshot, name="screenshot", attachment_type=allure.attachment_type.PNG)
--- tests/test_google.py
+++ tests/test_google.py
@@ -1,46 +1,35 @@
-import os
 import random
-import pytest
-
-from selenium import webdriver
+import allure
 from selenium.webdriver.common.keys import Keys
 
 
-@pytest.fixture
-def driver():
-    options = webdriver.ChromeOptions()
-    # options.add_argument("headless")  # Headless mode - browser with UI (faster)
-    # options.add_experimental_option("detach", True)  # Leave browser open
-
-    # Connect to Selenium Hub
-    # Instead of localhost, change it to the name of the server running Selenium hub
-    if 'SELENIUM_URL' in os.environ:
-        hub_url = os.environ['SELENIUM_URL']
-        print(f"Hub URL: {hub_url}")
-        driver = webdriver.Remote(command_executor=hub_url, options=options)
-    else:
-        driver = webdriver.Chrome(options=options)
-
-    yield driver
-    driver.quit()
-
-
+@allure.feature('Navigation')
+@allure.story('Open Example Domain')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_google(driver):
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    with allure.step('Open Google'):
+        driver.get('https://www.google.com')
 
-    # Find the search box by name attribute and type a random topic
-    search_box = driver.find_element("name", "q")
+    with allure.step("Verify the page title"):
+        assert "Google" in driver.title
 
-    # Example list of random topics
-    topics = ["Python programming", "Machine learning", "Space exploration", "Artificial intelligence"]
+    with allure.step("Search from a list of random topics"):
+        # Find the search box by name attribute and type a random topic
+        search_box = driver.find_element("name", "q")
 
-    # Choose a random topic from the list
-    random_topic = random.choice(topics)
+        # Example list of random topics
+        topics = ["Python programming", "Machine learning", "Space exploration", "Artificial intelligence"]
 
-    # Type the random topic into the search box and press Enter
-    search_box.send_keys(random_topic)
-    search_box.send_keys(Keys.RETURN)
+        # Choose a random topic from the list
+        random_topic = random.choice(topics)
+
+        # Type the random topic into the search box and press Enter
+        search_box.send_keys(random_topic)
+        search_box.send_keys(Keys.RETURN)
 
     # Wait for a few seconds to see the results
     driver.implicitly_wait(5)
+
+    # Take a screenshot
+    screenshot = driver.get_screenshot_as_png()
+    allure.attach(screenshot, name="screenshot", attachment_type=allure.attachment_type.PNG)
