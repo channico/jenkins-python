7abc55faa41e45cd4b84d221684f47c19c38a95a
Nicodemus Chan <nicodemus@phonester.com>
2024-07-03 23:37:17 +0800

    Remove print hub_url statement and set it to headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,14 +12,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 def driver():
     # Initialize the WebDriver
     options = webdriver.ChromeOptions()
-    # options.add_argument("headless")  # Headless mode - browser with UI (faster)
+    options.add_argument("headless")  # Headless mode - browser with UI (faster)
     # options.add_experimental_option("detach", True)  # Leave browser open
 
     # Connect to Selenium Hub
     # Instead of localhost, change it to the name of the server running Selenium hub
     if 'SELENIUM_URL' in os.environ:
         hub_url = os.environ['SELENIUM_URL']
-        print(f"Hub URL: {hub_url}")
         driver = webdriver.Remote(command_executor=hub_url, options=options)
     else:
         driver = webdriver.Chrome(options=options)
